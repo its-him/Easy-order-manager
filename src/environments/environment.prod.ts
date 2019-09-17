5504f5ae7f447400c748d619197b9bd064307569
Himanshu <himanshupednekar036@gmail.com>
2019-09-17 18:55:04 +0530

    hoting added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firsbaseConfig: {
+    apiKey: "AIzaSyDNuh4dPY99Uxqq4VWCnm6jDDO9kiliDG0",
+    authDomain: "too-easy-order.firebaseapp.com",
+    databaseURL: "https://too-easy-order.firebaseio.com",
+    projectId: "too-easy-order",
+    storageBucket: "too-easy-order.appspot.com",
+    messagingSenderId: "417271147874",
+    appId: "1:417271147874:web:60c77bafe4a513ab"
+  }
 };
